eceb192f03898672eec0a5cb3aae45825038877c
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-03-10 11:09:39 -0300

    finalizando aula de comentários

---
+++ fundamentos/src/comentario/Comentarios.java
@@ -0,0 +1,34 @@
+package comentario;
+
+
+/**
+ * Essa classe representa... (JAVADOC)
+ *
+ * @author Gleisson Braga <@email.com>
+ * @since JDK1.0
+ * @see
+ */
+
+public class Comentarios {
+
+    /**
+     *
+     * @param args
+     *
+     */
+
+    public static void main(String[] args) {
+        // A code sentence ends with (;)
+        // Mais uma linha...
+        // Fim
+        System.out.println("Primeiro programa!!!");
+
+        /*
+          * Linha 1
+          * Linha 2
+          * Linha 3
+        */
+
+
+    }
+}
